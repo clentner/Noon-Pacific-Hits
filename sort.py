fcb2e01be563c0818224c7248d26a688f9037f6f
Chris Lentner <cjl8505@rit.edu>
2016-07-27 16:33:58 -0400

    Cleanup

--- sort.py
+++ sort.py
@@ -1,23 +1,18 @@
 '''
-Print a markdown table of the top tracks.
+Print a markdown table of the top tracks by YouTube view count.
 '''
-
 import json
 
 with open('np-200-tracks.json', encoding='utf8') as f:
     tracks = json.load(f)
 
 for i, track in enumerate(tracks):
-    if 'listens' in track:
-        tracks[i]['listens'] = int(track['listens'])
-    else:
+    if 'listens' not in track:
         tracks[i]['listens'] = 0
 
 sorted_tracks = sorted(tracks, key=lambda t: t['listens'], reverse = True)
 top = sorted_tracks[:10]
-top_pretty = ['{} | {} | {}'.format(t['listens'], t['artist'], t['title'])
-                for t in top]
 print('Views | Artist | Title \n' +
       '----- | ------ | -----')
-for s in top_pretty:
-    print(s)
+for t in top:
+    print('{} | {} | {}'.format(t['listens'], t['artist'], t['title']))
