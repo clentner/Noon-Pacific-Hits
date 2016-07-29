06db09b56c25999aa9ec3deb495d135ea6f8d415
Chris Lentner <cjl8505@rit.edu>
2016-07-28 23:11:33 -0400

    refactor

--- sort.py
+++ sort.py
@@ -2,31 +2,48 @@
 Print a markdown table of the top tracks by YouTube view count.
 '''
 import json
+import sys
 
-with open('np-200-tracks.json', encoding='utf8') as f:
-    tracks = json.load(f)
+def sorted_tracks(tracks):
+    for i, track in enumerate(tracks):
+        if 'listens' not in track:
+            tracks[i]['listens'] = 0
 
-with open('np-200.json', encoding='utf8') as f:
-    npdata = json.load(f) # list of dicts containing lists of dicts
+    return sorted(tracks, key=lambda t: t['listens'], reverse = True)
 
-titles = {}
-for mix in npdata:
-    titles[str(mix['id'])] = mix['title']
+def markdown_tracklist(top, npdata):
+    mixtapes = {}
+    for mix in npdata:
+        mixtapes[mix['id']] = mix
+        
+    markdown = ('Views | Artist | Title | Playlist\n' +
+                '----- | ------ | ----- | --------\n')
+    for t in top:
+        table_row = '[{:,}](https://youtube.com/watch?v={}) | {} | {} | [{}](http://beta.noonpacific.com/#/weekly/{}/{})'.format(
+            t['listens'],
+            t['video_id'],
+            t['artist'],
+            t['title'],
+            mixtapes[t['mixtape']]['title'],
+            mixtapes[t['mixtape']]['slug'],
+            t['slug'])
+        markdown += table_row + '\n'
 
-for i, track in enumerate(tracks):
-    if 'listens' not in track:
-        tracks[i]['listens'] = 0
+    return markdown
 
-sorted_tracks = sorted(tracks, key=lambda t: t['listens'], reverse = True)
-top = sorted_tracks[:10]
-print('Views | Artist | Title | Playlist\n' +
-      '----- | ------ | ----- | --------')
-for t in top:
-    table_row = '[{:,}](https://youtube.com/watch?v={}) | {} | {} | [{}](http://noonpacific.com/#/mix/{})'.format(
-        t['listens'],
-        t['video_id'],
-        t['artist'],
-        t['title'],
-        titles[str(t['mixtape'])],
-        t['mixtape'])
-    print(table_row)
+def main():
+    if len(sys.argv) > 1:
+        num_tracks = int(sys.argv[1])
+    else:
+        num_tracks = 10
+
+    with open('np-200-tracks.json', encoding='utf8') as f:
+        tracks = json.load(f)
+    with open('np-200.json', encoding='utf8') as f:
+        npdata = json.load(f) # list of dicts containing lists of dicts
+
+    st = sorted_tracks(tracks)[:num_tracks]
+    print(markdown_tracklist(st, npdata))
+
+if __name__ == '__main__':
+    main()
